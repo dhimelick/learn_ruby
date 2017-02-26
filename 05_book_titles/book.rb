67e132ecf3bcde3a8e7c375fd4ca2b13c4de9230
David Himelick <david.himelick@gmail.com>
2017-02-26 15:58:26 -0600

    Complete 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
 # write your code here
+    def initialize
+        @title = ""
+    end
+
+    def title
+        @title
+    end
+
+    def title=(words)
+        @title = titleize(words)
+    end
+
+    def titleize(words)
+        stop_words = ["a", "an", "the", "and", "of", "in"]
+        words.capitalize!
+        words.split(" ").map! do |word|
+            if stop_words.include?(word)
+                word
+            else
+                word.capitalize
+            end
+        end.join(" ")
+    end
 end
