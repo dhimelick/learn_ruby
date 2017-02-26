f6d88d84f2f764b23aa73e9dcf4c66aa5d9a5eb7
David Himelick <david.himelick@gmail.com>
2017-02-26 16:23:44 -0600

    Complete 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+      @seconds = 0
+  end
+
+  def time_string
+      sec = @seconds % 60
+      min = @seconds / 60 % 60
+      hr = @seconds / 3600 % 60
+      hr_s = hr.to_s.rjust(2, "0")
+      min_s = min.to_s.rjust(2, "0")
+      sec_s = sec.to_s.rjust(2, "0")
+      "#{hr_s}:#{min_s}:#{sec_s}"
+  end
 end
